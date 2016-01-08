9be95ad0b2227fbbf3d7d2ae1da16674b244842c
rPawel <p.ruchaj@yahoo.co.uk>
2016-01-08 00:34:05 +0000

    run

--- run.sh
+++ run.sh
@@ -3,12 +3,15 @@ PERSISTENT_CONFIG_FOLDER="/root/.persistent-config"
 PERSISTENT_IGNORED_CONFIG_FOLDER=$PERSISTENT_CONFIG_FOLDER/.ignore
 VOLATILE_CONFIG_FOLDER="/"
 
+if [ ! -d /var/www/app ]; then
+    mkdir -p /var/www/app/public
+    chown -R user:www-data /var/www/app
+fi
 rm -rf /home; ln -s /var/www/app /home
-
-mkdir -p /var/log/php5 /var/log/apache2 /var/www/app/public
+mkdir -p /var/log/php5 /var/log/apache2
 chmod 775 /var/log/php5 /var/log/apache2
 find /var/log/php5 /var/log/apache2 -type f -exec chmod 644 {} \;
-chown -R user:www-data /var/log/php5 /var/log/apache2 /var/www/app
+chown -R user:www-data /var/log/php5 /var/log/apache2
 
 cp -ar ${PERSISTENT_CONFIG_FOLDER}/* ${VOLATILE_CONFIG_FOLDER}
 
